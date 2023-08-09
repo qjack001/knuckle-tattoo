60aaaabde080045b8a71a3fe926f425a798bb905
Jack Guinane <jackguinane@gmail.com>
2023-08-08 19:01:31 -0700

    Create main page

---
+++ app.vue
@@ -0,0 +1,39 @@
+<template>
+	<Head>
+		<Meta charset="utf-8"/>
+		<Meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+		<Meta content="text/html; charset=utf-8" http-equiv="content-type"/>
+		<Meta name="theme-color" content="#fff"/>
+		<Title>Knuckle Tats</Title>
+	</Head>
+	<div class="app">
+		<NuxtPage/>
+	</div>
+</template>
+
+<style>
+	@font-face
+	{
+		font-family: 'Sailor Fat Tattoo';
+		src: url('/font/Sailors-Fat-Tattoo-Script.woff2');
+	}
+
+	body
+	{
+		margin: 0;
+		background: white;
+		font-family: 'Sailor Fat Tattoo', serif;
+	}
+
+	.screen-reader-only
+	{
+		clip: rect(1px, 1px, 1px, 1px);
+		clip-path: inset(50%);
+		height: 1px;
+		width: 1px;
+		margin: -1px;
+		overflow: hidden;
+		padding: 0;
+		position: absolute;
+	}
+</style>
