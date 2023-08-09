6ac27479c609ed38d0ef8ccbe371b5107e6e1074
Jack Guinane <jackguinane@gmail.com>
2023-08-08 18:59:06 -0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,1 +1,10 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+	ssr: true,
+	app: {
+		layoutTransition: false,
+		pageTransition: false,
+	},
 	routeRules: { '/': { redirect: '/love/hate' }},
+})
